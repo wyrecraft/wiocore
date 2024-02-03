f70c29d27f0e02c812dbc150ca63a4c2976a4096
noxdev01 <noxdev01@gmail.com>
2024-02-03 21:20:08 +0200

    Filter rss posts by date

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,9 @@ import { getCollection } from 'astro:content';
 import { CONFIG } from "../config";
 
 export async function GET(context) {
-	const posts = await getCollection('posts');
+	const posts = (await getCollection('posts')).sort(
+		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+	);
 	return rss({
 		title: CONFIG.title,
 		description: CONFIG.description,
