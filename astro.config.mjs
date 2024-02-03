3bb04a15936c52d48239527a84679756c867758e
noxdev01 <noxdev01@gmail.com>
2024-02-04 00:27:36 +0200

    Add partytown in order to run google analytics code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import { CONFIG } from "./src/config";
 import sitemap from "@astrojs/sitemap";
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
 	site: CONFIG.url,
@@ -10,6 +11,11 @@ export default defineConfig({
 				page !== `${CONFIG.url}/thanks/` &&
         		page !== `${CONFIG.url}/confirmed/` &&
 				page !== `${CONFIG.url}/contact/thanks/`,
-		})
+		}),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
 	],
 });
