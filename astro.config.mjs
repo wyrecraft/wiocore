010d0fbf05710eb1f17e49eab5dd79251ecd37cb
noxdev01 <noxdev01@gmail.com>
2024-02-03 21:21:12 +0200

    Integrate sitemap and filter out some pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config';
 import { CONFIG } from "./src/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
 	site: CONFIG.url,
+	integrations: [
+		sitemap({
+			filter: (page) =>
+				page !== `${CONFIG.url}/thanks/` &&
+        		page !== `${CONFIG.url}/confirmed/` &&
+				page !== `${CONFIG.url}/contact/thanks/`,
+		})
+	],
 });
